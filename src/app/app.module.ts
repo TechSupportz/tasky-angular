886cbc81fd44934972576344eeb3ce591a53eca8
Nitish <tnitish654@gmail.com>
2022-06-05 18:22:52 +0800

    installed primeNg component library

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PrimengModule } from '../primeng.module';
+
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, PrimengModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/primeng.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { CalendarModule } from 'primeng/calendar';
+import { CardModule } from 'primeng/card';
+import { CheckboxModule } from 'primeng/checkbox';
+import { DropdownModule } from 'primeng/dropdown';
+import { InputTextModule } from 'primeng/inputtext';
+import { KeyFilterModule } from 'primeng/keyfilter';
+import { PasswordModule } from 'primeng/password';
+import { RadioButtonModule } from 'primeng/radiobutton';
+import { DividerModule } from 'primeng/divider';
+import { DynamicDialogModule } from 'primeng/dynamicdialog';
+import { TooltipModule } from 'primeng/tooltip';
+import { ToastModule } from 'primeng/toast';
+import { AvatarModule } from 'primeng/avatar';
+import { AvatarGroupModule } from 'primeng/avatargroup';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
+import { SkeletonModule } from 'primeng/skeleton';
+
+@NgModule({
+  exports: [
+    ButtonModule,
+    CardModule,
+    CalendarModule,
+    CheckboxModule,
+    DropdownModule,
+    InputTextModule,
+    KeyFilterModule,
+    PasswordModule,
+    RadioButtonModule,
+    DividerModule,
+    DynamicDialogModule,
+    TooltipModule,
+    ToastModule,
+    AvatarModule,
+    AvatarGroupModule,
+    ProgressSpinnerModule,
+    SkeletonModule,
+  ],
+})
+export class PrimengModule {}
